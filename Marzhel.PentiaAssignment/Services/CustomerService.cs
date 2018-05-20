0806fd21e10860ee194e409a136e7d13e88082ac
Mark Skov Jensen <msj004@me.com>
2018-05-20 11:44:51 +0200

    Added CustomerService

---
+++ Marzhel.PentiaAssignment/Services/CustomerService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using Marzhel.PentiaAssignment.Contexts;
+using Marzhel.PentiaAssignment.Models;
+
+namespace Marzhel.PentiaAssignment.Services
+{
+    public class CustomerService : ICustomerService, IDisposable
+    {
+        private readonly SalesContext _salesContext;
+
+        public CustomerService(IDbContextFactory<SalesContext> salesContextFactory)
+        {
+            _salesContext = salesContextFactory.Create();
+        }
+
+        public void Dispose()
+        {
+            _salesContext.Dispose();
+        }
+
+        public async Task<ICollection<Customer>> GetAsync()
+        {
+            return await _salesContext.Customers.ToListAsync();
+        }
+
+        public Task<Customer> GetAsync(int id)
+        {
+            return _salesContext.Customers.FirstOrDefaultAsync(customer => customer.CustomerId == id);
+        }
+
+        public async Task<ICollection<Customer>> GetByStreetAsync(string street)
+        {
+            var query = from customer in _salesContext.Customers
+                        where customer.Address.Line1 == street
+                        select customer;
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<ICollection<Customer>> GetByCarMake(string carMake)
+        {
+            var query = from purchase in _salesContext.CarPurchases
+                        where purchase.Car.Make == carMake
+                        select purchase.Customer;
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<ICollection<Customer>> GetByCarModel(string carModel)
+        {
+            var query = from purchase in _salesContext.CarPurchases
+                        where purchase.Car.Model == carModel
+                        select purchase.Customer;
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<ICollection<Customer>> GetByNameAsync(string name)
+        {
+            var query = from customer in _salesContext.Customers
+                        where customer.Name == name
+                        select customer;
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<ICollection<Customer>> GetBySalesPersonName(string salesPersonName)
+        {
+            var query = from purchase in _salesContext.CarPurchases
+                        where purchase.SalesPerson.Name == salesPersonName
+                        select purchase.Customer;
+
+            return await query.ToListAsync();
+        }
+    }
+}
---
+++ Marzhel.PentiaAssignment/Services/ICustomerService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Marzhel.PentiaAssignment.Models;
+
+namespace Marzhel.PentiaAssignment.Services
+{
+    public interface ICustomerService
+    {
+        Task<ICollection<Customer>> GetAsync();
+
+        Task<Customer> GetAsync(int id);
+
+        Task<ICollection<Customer>> GetByNameAsync(string name);
+
+        Task<ICollection<Customer>> GetByStreetAsync(string street);
+
+        Task<ICollection<Customer>> GetByCarModel(string carModel);
+
+        Task<ICollection<Customer>> GetByCarMake(string carMake);
+
+        Task<ICollection<Customer>> GetBySalesPersonName(string salesPersonName);
+    }
+}
