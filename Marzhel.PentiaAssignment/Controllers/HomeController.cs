4d0918c284787e79256b11971ee446a75803d0fd
Mark Skov Jensen <msj004@me.com>
2018-05-20 11:47:14 +0200

    Removed actions from HomeController

--- Marzhel.PentiaAssignment/Controllers/HomeController.cs
+++ Marzhel.PentiaAssignment/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,22 +9,8 @@ namespace Marzhel.PentiaAssignment.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
