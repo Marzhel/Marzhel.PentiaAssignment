f63c896f2437ef91f3ec43419fc5ab0389e62908
Mark Skov Jensen <msj004@me.com>
2018-05-20 11:42:34 +0200

    Added Entity Framework context for sales

---
+++ Marzhel.PentiaAssignment/Contexts/SalesContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using Marzhel.PentiaAssignment.Models;
+
+namespace Marzhel.PentiaAssignment.Contexts
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
+
+        public IDbSet<Customer> Customers { get; set; }
+        public IDbSet<CarPurchase> CarPurchases { get; set; }
+        public IDbSet<SalesPerson> SalesPeople { get; set; }
+        public IDbSet<Car> Cars { get; set; }
+        public IDbSet<CarExtra> CarExtras { get; set; }
+    }
+}
---
+++ Marzhel.PentiaAssignment/Contexts/SalesContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity.Infrastructure;
+
+namespace Marzhel.PentiaAssignment.Contexts
+{
+    public class SalesContextFactory : IDbContextFactory<SalesContext>
+    {
+        public SalesContext Create()
+        {
+            return new SalesContext("DefaultConnection");
+        }
+    }
+}
