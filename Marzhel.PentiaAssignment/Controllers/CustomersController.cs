faf605648e02ff70495bbd8af22a23ff7dfd7164
Mark Skov Jensen <msj004@me.com>
2018-05-20 13:18:29 +0200

    Added views of the diferent searches

--- Marzhel.PentiaAssignment/Controllers/CustomersController.cs
+++ Marzhel.PentiaAssignment/Controllers/CustomersController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
 using Marzhel.PentiaAssignment.Services;
 
@@ -20,6 +16,47 @@ namespace Marzhel.PentiaAssignment.Controllers
         public async Task<ActionResult> Index()
         {
             var customers = await _customerService.GetAsync();
+
+            return View(customers);
+        }
+
+        public async Task<ActionResult> ByName(string name)
+        {
+            if (name == null) { return View(); }
+            var customers = await _customerService.GetByNameAsync(name);
+
+            return View(customers);
+        }
+
+        public async Task<ActionResult> ByStreet(string street)
+        {
+            if (street == null) { return View(); }
+            var customers = await _customerService.GetByStreetAsync(street);
+
+            return View(customers);
+        }
+
+        public async Task<ActionResult> ByPurchaseOfCarMake(string make)
+        {
+            if (make == null) { return View(); }
+            var customers = await _customerService.GetByCarMakeAsync(make);
+
+            return View(customers);
+        }
+
+        public async Task<ActionResult> ByPurchaseOfCarModel(string model)
+        {
+            if (model == null) { return View(); }
+            var customers = await _customerService.GetByCarModelAsync(model);
+
+            return View(customers);
+        }
+
+        public async Task<ActionResult> BySalesPersonName(string salesPersonName)
+        {
+            if (salesPersonName == null) { return View(); }
+
+            var customers = await _customerService.GetBySalesPersonNameAsync(salesPersonName);
 
             return View(customers);
         }
