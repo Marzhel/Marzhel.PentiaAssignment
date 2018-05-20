bc02b635be918a308869a6b2b2c1f00ca6b7895b
Mark Skov Jensen <msj004@me.com>
2018-05-20 12:03:58 +0200

    Added start of customers controller

---
+++ Marzhel.PentiaAssignment/Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Marzhel.PentiaAssignment.Services;
+
+namespace Marzhel.PentiaAssignment.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomersController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var customers = await _customerService.GetAsync();
+
+            return View(customers);
+        }
+
+        public async Task<ActionResult> Details(int id)
+        {
+            var customer = await _customerService.GetAsync(id);
+
+            return View(customer);
+        }
+    }
+}
