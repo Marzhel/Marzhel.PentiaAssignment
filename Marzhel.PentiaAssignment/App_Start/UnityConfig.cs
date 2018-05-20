7ab51dc2eb3a97fef8299e5a3d88c2337dcc02e4
Mark Skov Jensen <msj004@me.com>
2018-05-20 11:46:03 +0200

    Configured Unity for customer service and EF context factory

--- Marzhel.PentiaAssignment/App_Start/UnityConfig.cs
+++ Marzhel.PentiaAssignment/App_Start/UnityConfig.cs
@@ -1,6 +1,10 @@
 using System;
-
+using System.Configuration;
+using System.Data.Entity.Infrastructure;
+using Marzhel.PentiaAssignment.Contexts;
+using Marzhel.PentiaAssignment.Services;
 using Unity;
+using Unity.AspNet.Mvc;
 
 namespace Marzhel.PentiaAssignment
 {
@@ -10,6 +14,7 @@ namespace Marzhel.PentiaAssignment
     public static class UnityConfig
     {
         #region Unity Container
+
         private static Lazy<IUnityContainer> container =
           new Lazy<IUnityContainer>(() =>
           {
@@ -22,26 +27,26 @@ namespace Marzhel.PentiaAssignment
         /// Configured Unity Container.
         /// </summary>
         public static IUnityContainer Container => container.Value;
-        #endregion
+
+        #endregion Unity Container
 
         /// <summary>
         /// Registers the type mappings with the Unity container.
         /// </summary>
         /// <param name="container">The unity container to configure.</param>
         /// <remarks>
-        /// There is no need to register concrete types such as controllers or
-        /// API controllers (unless you want to change the defaults), as Unity
-        /// allows resolving a concrete type even if it was not previously
-        /// registered.
+        /// There is no need to register concrete types such as controllers or API controllers
+        /// (unless you want to change the defaults), as Unity allows resolving a concrete type even
+        /// if it was not previously registered.
         /// </remarks>
         public static void RegisterTypes(IUnityContainer container)
         {
-            // NOTE: To load from web.config uncomment the line below.
-            // Make sure to add a Unity.Configuration to the using statements.
-            // container.LoadConfiguration();
+            // NOTE: To load from web.config uncomment the line below. Make sure to add a
+            // Unity.Configuration to the using statements. container.LoadConfiguration();
 
-            // TODO: Register your type's mappings here.
-            // container.RegisterType<IProductRepository, ProductRepository>();
+            container.RegisterType<ICustomerService, CustomerService>(new PerRequestLifetimeManager());
+
+            container.RegisterType<IDbContextFactory<SalesContext>, SalesContextFactory>();
         }
     }
 }
